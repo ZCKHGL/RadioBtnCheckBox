b84d9eae9144f51a5c05ba48e9f1fb3467f1736c
ZCKHGL <164823089+ZCKHGL@users.noreply.github.com>
2024-11-04 11:33:21 +0700

    Add files via upload

---
+++ RadioBtnCheckBoxv2/RadioCheck/src/main/java/com/mycompany/radiocheck/RadioCheck.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.radiocheck;
+
+/**
+ *
+ * @author D-12
+ */
+public class RadioCheck {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
